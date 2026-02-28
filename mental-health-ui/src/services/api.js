5c7b36d86cb6e950fe53f7659d1bc23cecf23c5e
leileideguzman96-arch <leileideguzman96@gmail.com>
2026-02-28 22:51:20 +0800

    api.js fix

--- mental-health-ui/src/services/api.js
+++ mental-health-ui/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Direct connection to your live Render Backend
-const API_URL = 'https://mood-tracker-ai-4u98.onrender.com';
+const API_URL = 'https://lab5-b4qk.onrender.com';
 
 export default axios.create({
   baseURL: API_URL
